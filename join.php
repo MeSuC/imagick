3145a37e4871529018840aa7726438255048c610
DESKTOP-CMJAC51\ndsks <microbug7@gmail.com>
2018-05-28 15:24:37 +0900

    コードの修正

--- join.php
+++ join.php
@@ -1,9 +1,12 @@
 <?php
-$image1 = new Imagick('food1.png');
-$image2 = new Imagick('food2.png');
+$sprite1=$_GET['file1'];
+$sprite2=$_GET['file2'];
+$image1 = new Imagick($sprite1);
+$image2 = new Imagick($sprite2);
 
 // ‡¬
 $image1->compositeImage( $image2, imagick::COMPOSITE_DEFAULT, 0, 0 );
  
 header("Content-Type: image/png");
+ob_clean();
 echo $image1;
